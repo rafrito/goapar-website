db7ba6b3ed2cae7f1021234c19a0def7ceb64857
chouzzy <mathfernando@hotmail.com>
2025-07-11 17:43:47 -0300

    eslint desativado

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
-// /** @type {import("eslint").Linter.Config} */
-// const eslintConfig = {
-//   "off": "warn"
-// };
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
 
 // export default eslintConfig;
+// // /** @type {import("eslint").Linter.Config} */
+// // const eslintConfig = {
+// //   "off": "warn"
+// // };
+
+// // export default eslintConfig;
+
+export default []
