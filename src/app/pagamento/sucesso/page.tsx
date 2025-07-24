f315d3141c70591fe438265c60774e2e35547b43
chouzzy <mathfernando@hotmail.com>
2025-07-24 19:51:28 -0300

    download link atualizado

--- src/app/pagamento/sucesso/page.tsx
+++ src/app/pagamento/sucesso/page.tsx
@@ -64,7 +64,7 @@ export default function PaginaSucesso() {
     const MotionButton = motion(Button);
 
     // TODO: Adicione o link real para o download do seu aplicativo
-    const downloadLink = "https://release-assets.githubusercontent.com/github-production-release-asset/999249052/23d3411b-1193-4254-9b5c-6121eba5aa27?sp=r&sv=2018-11-09&sr=b&spr=https&se=2025-07-24T23%3A06%3A27Z&rscd=attachment%3B+filename%3DBoTRT-Setup-0.0.901.exe&rsct=application%2Foctet-stream&skoid=96c2d410-5711-43a1-aedd-ab1947aa7ab0&sktid=398a6654-997b-47e9-b12b-9515b896b4de&skt=2025-07-24T22%3A05%3A50Z&ske=2025-07-24T23%3A06%3A27Z&sks=b&skv=2018-11-09&sig=I0x51UuadOP422oS2UR74MYhLUwsBoepSMk9W8UlzWE%3D&jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmVsZWFzZS1hc3NldHMuZ2l0aHVidXNlcmNvbnRlbnQuY29tIiwia2V5Ijoia2V5MSIsImV4cCI6MTc1MzM5NTU0MCwibmJmIjoxNzUzMzk1MjQwLCJwYXRoIjoicmVsZWFzZWFzc2V0cHJvZHVjdGlvbi5ibG9iLmNvcmUud2luZG93cy5uZXQifQ.8_UDfI9RRWhslg1JvysUeXh_C867UVSDQ8Upb1ywX9U&response-content-disposition=attachment%3B%20filename%3DBoTRT-Setup-0.0.901.exe&response-content-type=application%2Foctet-stream";
+    const downloadLink = "https://drive.usercontent.google.com/download?id=1c0SrVPOCOoXvY9SavZxQHexEpwO5jf6i&export=download&authuser=0&confirm=t&uuid=b28a986d-7231-4ee9-90c5-5ad3dcb0e36b&at=AN8xHopSl39tLCNRWBX5Ef8A6edI:1753397455028";
 
     return (
         <MotionFlex
